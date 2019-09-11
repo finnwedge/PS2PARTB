f15b436b63bdac1c2141178c630e2384545e9abf
Finn Wedge <f.wedge@uq.net.au>
2019-09-11 16:45:17 +1000

    cleaned up a bit

--- strawberries.m
+++ strawberries.m
@@ -1,38 +1,32 @@
+ %% Set up workspace
  clc
  clear all
  close all
+
+ 
+ %% Importing image for card detection
  im1=imread('C:\Users\Finn\Dropbox\THIRD YEAR\METR4202\Sandbox\images\Simple19.png');
  figure(1);
- imshow(im1)
- im2 = rgb2gray(im1);
- [counts, locations] = imhist(im2)
- threshold = thresh(counts);
+ imshow(im1);
+ 
+ %% Pre-edge dectection image 
+ im2 = rgb2gray(im1); %creates a grayscale version of the image
+ [counts, locations] = imhist(im2) %creates a histogram of the image
+ % counts - number of pixels in location
+ % location range - 0-256
+ threshold = thresh(counts); %creates a threshold based on the histogram peaks
  %threshold = (t/256);
  %im2 = histeq(im2);
- figure(2);
- imshow(im2);
- im = edge(im2, 'canny', threshold);
- figure(3);
- imshow(im);
+ figure; imshow(im2); 
  
- se = strel('disk', 5,0);
+ %% Edge detection
+ im = edge(im2, 'canny', threshold); %detects the edges of the image using the canny method and the threshold calculated above
+ figure; imshow(im);
+ 
+ se = strel('disk', 5,0); %creates a structual element
  im = imdilate(im,se);
  figure; imshow(im)
  im = imerode(im,se);
  figure; imshow(im)
  im = imfill(im,'holes');
  figure; imshow(im)
-
- 
- 
-%  [im, radii] = imfindcircles(im, [1,100]);
-%  figure; imshow(im);
-%  viscircles(im, radii,'Color','b');
-%  
-%  [H,T,R] = hough(im,'RhoResolution',0.5,'Theta',-90:0.5:89.5);
-%  figure; imshow(imadjust(rescale(H)),'XData',T,'YData',R,'InitialMagnification','fit');
-%  
- %im = imerode(im,se)
- %figure; imshow(im)
-%  im2 = imfill(im);
-%  figure; imshow(im2)
