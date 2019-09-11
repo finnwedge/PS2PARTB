68047e849b039c6f0ecf9e0528129bbda0aba06f
Finn Wedge <f.wedge@uq.net.au>
2019-09-11 17:47:51 +1000

    fixed threshold function, but will need to isolate cards from other objects of similar colour. Working overlay function

--- strawberries.m
+++ strawberries.m
@@ -11,10 +11,14 @@
  
  %% Pre-edge dectection image 
  im2 = rgb2gray(im1); %creates a grayscale version of the image
- [counts, locations] = imhist(im2) %creates a histogram of the image
+ [counts, locations] = imhist(im2); %creates a histogram of the image
  % counts - number of pixels in location
  % location range - 0-256
- threshold = thresh(counts); %creates a threshold based on the histogram peaks
+ % threshold = 155/256;
+ % threshold = threshold(counts); %creates a threshold based on the histogram peaks
+ threshold_bin = double(min((multithresh(im2, 3))));
+ threshold = threshold_bin/255;
+ %threshold = double(threshold_bin(2)) / 255;
  %threshold = (t/256);
  %im2 = histeq(im2);
  figure; imshow(im2); 
@@ -23,10 +27,25 @@
  im = edge(im2, 'canny', threshold); %detects the edges of the image using the canny method and the threshold calculated above
  figure; imshow(im);
  
- se = strel('disk', 5,0); %creates a structual element
+ se = strel('disk', 10,0); %creates a structual element
  im = imdilate(im,se);
  figure; imshow(im)
  im = imerode(im,se);
  figure; imshow(im)
  im = imfill(im,'holes');
  figure; imshow(im)
+ 
+ %% Border Overlay
+ [B,L,n,A] = bwboundaries(im);
+ hold on
+ for k = 1:n
+    boundary = B{k};
+    plot(boundary(:,2), boundary(:,1), 'r','LineWidth',2);
+ end
+ 
+ figure; imshow(im1);
+ hold on
+  for k = 1:n
+    boundary = B{k};
+    plot(boundary(:,2), boundary(:,1), 'r','LineWidth',2);
+ end
