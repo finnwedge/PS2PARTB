dd111abb18aa0a7147cee3084e7defa2058f4963
Finn Wedge <f.wedge@uq.net.au>
2019-09-12 13:13:46 +1000

    almost working

--- strawberries.m
+++ strawberries.m
@@ -16,7 +16,7 @@
  % location range - 0-256
  % threshold = 155/256;
  % threshold = threshold(counts); %creates a threshold based on the histogram peaks
- threshold_bin = double(min((multithresh(im2, 3))));
+ threshold_bin = double(min((multithresh(im2, 5))));
  threshold = threshold_bin/255;
  %threshold = double(threshold_bin(2)) / 255;
  %threshold = (t/256);
@@ -40,14 +40,34 @@
  hold on
  for k = 1:n
     boundary = B{k};
-    plot(boundary(:,2), boundary(:,1), 'r','LineWidth',2);
+    plot(boundary(:,2), boundary(:,1), 'r','LineWidth',1);
  end
  
  figure; imshow(im1);
  hold on
   for k = 1:n
     boundary = B{k};
-    plot(boundary(:,2), boundary(:,1), 'r','LineWidth',2);
+    plot(boundary(:,2), boundary(:,1), 'r','LineWidth',1);
   end
  
- %% Making a small change
+ %% Isolation of cards
+ %  All cards are of size 56 x 87mm. Thus the aspect ratio is
+ %  1:1.55357142857. Allowing for 5% error:
+ aspect_ratio_range = [((87/56)-(87/56)*0.05), ((87/56)+(87/56)*0.05)];
+ %cards = bwpropfilt(im1, 
+ stats = regionprops(im,'MajorAxisLength','MinorAxisLength','PixelList','Image');
+ pixel_list={};
+ image_pixels = L(1)*L(2);
+ cards = bwareaopen(im,image_pixels);
+ figure; imshow(cards)
+ for k = 1:n
+     major = stats(k).MajorAxisLength;
+     minor = stats(k).MinorAxisLength;
+     aspect_ratio = major/minor;
+     if aspect_ratio >= aspect_ratio_range(1) && aspect_ratio <= aspect_ratio_range(2)
+         pixel_list(k,:) = {stats(k).PixelList(k,1),stats(k).PixelList(k,2)};
+         cards = imoverlay(cards,bwselect(im,pixel_list{k,1},pixel_list{k,2}));
+         figure; imshow(cards);
+     end
+     
+ end
